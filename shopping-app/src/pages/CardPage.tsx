afcf4dbbc6ec35f6332e586aeac5785e792d0cd7
Sara M <saralilialihania@gmail.com>
2025-06-05 10:36:25 -0400

    Update CardPage.tsx

--- shopping-app/src/pages/CardPage.tsx
+++ shopping-app/src/pages/CardPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Navbar from '@/components/Navbar';
+import Navbar from '../components/NavBar';
 import Footer from '@/components/Footer';
 import { useCartStore } from '@/stores';
 import { Button } from '@/components/ui/button';
